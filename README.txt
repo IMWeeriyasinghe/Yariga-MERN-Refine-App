93726d9f3e8e8c4809c3be8d1886ef38030597b1
IMWeeriyasinghe <madusankaweeriyasinghe20@gmail.com>
2023-03-04 23:41:37 +0530

    Commit

---
+++ README.txt
@@ -0,0 +1,9 @@
+Content Creator Git Repos
+Server - https://github.com/refinedev/refine/blob/next/examples/mern-dashboard-server/controllers/user.controller.js
+Client - https://github.com/refinedev/refine/blob/next/examples/mern-dashboard-client/src/pages/home.tsx
+
+UIs Figma
+https://www.figma.com/file/QLU3mZJOsmnAN4SEQ8YSTA/Real-Estate-Admin-Dashboard?node-id=214%3A3266&t=y8JiD1SDSEYKn1Ht-0
+
+YouTube
+https://www.youtube.com/watch?v=k4lHXIzCEkM&t=10280s&ab_channel=JavaScriptMastery
--- client/src/App.tsx
+++ client/src/App.tsx
@@ -56,7 +56,7 @@ function App() {
       const profileObj = credential ? parseJwt(credential) : null;
 
       if (profileObj) {
-        const response = await fetch('http://localhost:8080/api/v1/users', {
+        const response = await fetch('https://im-yariga-mern-refine-app.onrender.com/api/v1/users', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
@@ -125,7 +125,7 @@ function App() {
       <GlobalStyles styles={{ html: { WebkitFontSmoothing: "auto" } }} />
       <RefineSnackbarProvider>
         <Refine
-          dataProvider={dataProvider("http://localhost:8080/api/v1")}
+          dataProvider={dataProvider("https://im-yariga-mern-refine-app.onrender.com/api/v1")}
           notificationProvider={notificationProvider}
           ReadyPage={ReadyPage}
           catchAll={<ErrorComponent />}
